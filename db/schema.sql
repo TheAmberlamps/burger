333dfa1b99401bc77d1551a105661d4576d9a9a0
Paul Unsted <ed__84@hotmail.com>
2019-09-02 07:14:12 -0400

    Gradually working my way through the orm, time for a commit and a break.

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS todolist;
-CREATE DATABASE todolist;
-USE todolist;
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 CREATE TABLE burgers(
   id int NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(255) NOT NULL,
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ("Homeburger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Deluxe Homeburger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Pizzaburger", 0);
