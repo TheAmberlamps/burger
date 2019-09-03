1b8583d9bf2576516287b4125635360930db8a46
Paul Unsted <ed__84@hotmail.com>
2019-09-03 05:53:55 -0400

    Filled-in loads of code, time for a break.

--- config/orm.js
+++ config/orm.js
@@ -5,7 +5,26 @@ var orm = {
         var queryString = "SELECT * FROM ??";
         connection.query(queryString, [table], 
             function(err, result)  {
-            if (err) throw err;
-            console.log(result);
+              if (err) throw err;
+              console.log(result);
         });
-    }};
+    },
+    insertOne: function(table, newBurg)  {
+        var queryString = "INSERT INTO ?? VALUES ??";
+        connection.query(queryString, [table, newBurg],
+            function(err, result)  {
+              if (err) throw err;
+              console.log(result);
+        });
+    },
+    updateOne: function(whatBurg, table) {
+        var queryString = "SELECT ?? FROM ??";
+        connection.query(queryString, [whatBurg, table],
+            function(err, result)   {
+              if (err) throw err;
+              console.log(result);
+        });
+    }
+};
+
+module.exports(orm);
