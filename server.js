3e5f49d8fe158fa3498a65dea63c17ae101af539
Paul Unsted <ed__84@hotmail.com>
2019-09-05 05:22:34 -0400

    minor tweaks to help track file pathing

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require("express");
-
-var PORT = process.env.PORT || 8080;
+var exphbs = require("express-handlebars");
 
 var app = express();
+
+var PORT = process.env.PORT || 8080;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -11,14 +12,11 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_Controller.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
