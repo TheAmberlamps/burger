b04a077079b6d4e991d61e87efebd2c7c490a33e
Paul Unsted <ed__84@hotmail.com>
2019-10-12 20:39:26 -0400

    Hooked project with Jawsdb

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "3Dd13m4n",
+    password: "br4nch&st3m",
     database: "burgers_db"
   });
 }
